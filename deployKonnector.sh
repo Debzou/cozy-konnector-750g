ffdb2385319f6b3ca3b3e811a9c81f9f5cd2810e
debzou <david.petit@etu.univ-smb.fr>
2020-06-19 09:18:54 +0200

    package json deploy

--- deployKonnector.sh
+++
@@ -1,4 +0,0 @@
-npm_package_repository_url = "https://githubhttps://github.com/Debzou/cozy-konnector-750g/blob/master/package.json.com/" \n
-DEPLOY_BRANCH="master" \n
-DEPLOY_REPOSITORY="cozy-konnector-750g" \n
-git-directory-deploy --directory build/ --branch ${DEPLOY_BRANCH:-build} --repo=${DEPLOY_REPOSITORY:-$npm_package_repository_url}
